fcb73a5e551a5415fbb2998d1f489837a12f8082
LuizLamim <luiz.britohenrique@hotmail.com>
2022-10-01 19:41:07 -0300

    Número par até 100

---
+++ prov.py
@@ -0,0 +1,6 @@
+num1 = int(input('digite um número:'))
+num2 = int(input('digite um número:'))
+resultado = num1/num2
+
+resultado = num1/num2
+print('A divisão entre {} e {} é igual a {}'.format(num1, num2, resultado)) 
---
+++ prov2.py
@@ -0,0 +1,4 @@
+cont = 0
+while cont<100:
+    cont = cont + 2
+    print("Números pares até 100 {}".format(cont))
